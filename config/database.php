48cf2a41674665f4747d533b55b85aba459e206e
JacobMeister <maarten.jacobs15@gmail.com>
2018-03-24 23:01:07 +0100

    l

--- config/database.php
+++ config/database.php
@@ -1,5 +1,21 @@
 <?php
 
+$host = env('DB_HOST', '127.0.0.1');
+$database = env('DB_DATABASE', '');
+$username = env('DB_USERNAME', 'forge');
+$password = env('DB_PASSWORD', 'forge');
+
+
+if($databaseUrl = getenv('DATABASE_URL')) {
+    
+    $url = parse_url($databaseUrl);
+    
+    $host = $url['host'];
+    $username = $url['user'];
+    $password = $url['pass'];
+    $database = substr($url['path'], 1);
+}
+
 return [
 
     /*
@@ -56,16 +72,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
+        ]
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
